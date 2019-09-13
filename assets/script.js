7bc70bba9bea94e24d11921cf11d89d72dbb5e35
alic3579 <pany468@newschool.edu>
2019-09-13 12:30:17 -0400

    Update

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
 var myWrapper = document.getElementById("wrapper");
-var myWrapperTwo = document.getElementById("wrapperTwo");
+var myContent = document.getElementById("content");
 
 
 
 for (var i = 0; i < 200; i++) {
 
-	myWrapper.innerHTML += '<div class="patternUnit" style=" border: 5px solid rgb('+(255 - i)+','+(170 + (i*3))+', '+(200 + (i+1))+'); transform: rotate('+ (200 * i) +'deg);"></div>';
+	myWrapper.innerHTML += '<div class="patternUnit" style=" border: 5px solid rgb('+(0 + i)+', 200, '+(255 - i)+');"></div>';
 }
 
 
 // border-radius: ('+(50 + (i * 8))+'px); transform: scale('+ (5* i) +');
+
+
+
+for (var j = 0; j < 160; j++) {
+
+	myContent.innerHTML += '<div class="contentUnit" style=" border: 5px solid rgb('+(0 + j)+', 0, '+(255 - j)+'); transform: rotate('+ (180 * j) +'deg);"></div>';
+}
